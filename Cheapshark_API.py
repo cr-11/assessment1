366fe627afee49fb78f2e5eef9a8ed7ea90ef5bf
Cristopher A. Gonzalez <42201664+crist-1@users.noreply.github.com>
2023-08-25 00:44:43 +0100

    Update Cheapshark_API.py
    Added the option_2, search_by_name.

--- Cheapshark_API.py
+++ Cheapshark_API.py
@@ -22,9 +22,35 @@ def game_deals():                   #API GET code
     else:
         print("Failed to retrieve game deals. Status code:", response.status_code)
 
-def search_by_name():
-    name = input("Enter the game name: ")
-   
+# Function to search for matching deals by game name
+def search_by_name(game_name, deals):
+    matching_deals = []
+    for deal in deals:
+        #If the game name provided by the user (ignoring uppercase/lowercase because the next line code will convert any typed text in lower text and it will compare with the game in the deals) is in the deal's title
+        if game_name.lower() in deal["title"].lower():
+            matching_deals.append(deal)  # This will add to the list of matching deals
+    return matching_deals  # Return the list of matching deals
+
+        elif choice == '2':
+            print("Random Games with Game Name:")
+            random.shuffle(deals)  # Gives some examples for random games
+            for _ in range(5): #The maximun number of games that will show will be 5.
+                deal = random.choice(deals)  # Select a random deal
+                print("Game Name:", deal["title"])
+            
+            game_name = input("Enter the game name: ")
+            matching_deals = search_by_name(game_name, deals)
+            
+            if len(matching_deals) == 0:
+                print("No matching deals found.") #This will come out if any matching games was found
+            else:
+                for deal in matching_deals:
+                    print("Game Title:", deal["title"])
+                    print("Deal ID:", deal["dealID"])
+                    print("Game ID:", deal["gameID"])
+                    print("Normal Price:", deal["normalPrice"])
+                    print("Deal Price:", deal["salePrice"])
+                    print("-" * 30)
 
 def search_by_game_id():
     game_id = input("Enter the game ID: ")
